0fbe160774a996259bfd9a16e99d7f3c55191585
Monjur Khuda <kiatbrooklyn@gmail.com>
2021-09-30 13:01:03 -0400

    mikro-orm init

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [],
+        dbName: 'roadmap',
+        type: 'postgresql',
+        debug: !constants_1.__prod__,
+    });
+};
+main();
 console.log("hello");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+
+const main = async () => {    
+
+const orm = await MikroORM.init({
+    entities: [],
+    dbName:'roadmap',
+    type: 'postgresql',
+    debug: !__prod__,
+});
+
+}
+
+main()
+
 console.log("hello");
