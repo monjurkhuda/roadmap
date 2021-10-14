eaf297d1ea31f525da68f37c904f425d022be3ee
Monjur Khuda <kiatbrooklyn@gmail.com>
2021-10-14 16:08:45 -0400

    Register and Login resolvers setup

--- dist/resolvers/user.js
+++ dist/resolvers/user.js
@@ -32,25 +32,112 @@ __decorate([
 UsernamePasswordInput = __decorate([
     (0, type_graphql_1.InputType)()
 ], UsernamePasswordInput);
+let FieldError = class FieldError {
+};
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "field", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(),
+    __metadata("design:type", String)
+], FieldError.prototype, "message", void 0);
+FieldError = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], FieldError);
+let UserResponse = class UserResponse {
+};
+__decorate([
+    (0, type_graphql_1.Field)(() => [FieldError], { nullable: true }),
+    __metadata("design:type", Array)
+], UserResponse.prototype, "errors", void 0);
+__decorate([
+    (0, type_graphql_1.Field)(() => User_1.User, { nullable: true }),
+    __metadata("design:type", User_1.User)
+], UserResponse.prototype, "user", void 0);
+UserResponse = __decorate([
+    (0, type_graphql_1.ObjectType)()
+], UserResponse);
 let UserResolver = class UserResolver {
     async register(options, { em }) {
+        if (options.username.length <= 2) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "Username length must be greater than 2.",
+                    },
+                ],
+            };
+        }
+        if (options.password.length <= 8) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "Password length must be atleast 8 characters.",
+                    },
+                ],
+            };
+        }
         const hashedPassword = await argon2_1.default.hash(options.password);
         const user = em.create(User_1.User, {
-            username: options.username,
+            username: options.username.toLowerCase(),
             password: hashedPassword,
         });
+        try {
+            await em.persistAndFlush(user);
+        }
+        catch (err) {
+            if (err.code === "23505") {
+                return {
+                    errors: [
+                        {
+                            field: "username",
+                            message: "That username is not available.",
+                        },
+                    ],
+                };
+            }
+            console.log("message: ", err.message);
+        }
         await em.persistAndFlush(user);
-        return user;
+        return { user };
+    }
+    async login(options, { em }) {
+        const user = await em.findOne(User_1.User, {
+            username: options.username.toLowerCase(),
+        });
+        if (!user) {
+            return {
+                errors: [{ field: "username", message: "Invalid Login." }],
+            };
+        }
+        const valid = await argon2_1.default.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [{ field: "password", message: "Invalid Login." }],
+            };
+        }
+        return { user };
     }
 };
 __decorate([
-    (0, type_graphql_1.Mutation)(() => User_1.User),
+    (0, type_graphql_1.Mutation)(() => UserResponse),
     __param(0, (0, type_graphql_1.Arg)("options")),
     __param(1, (0, type_graphql_1.Ctx)()),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
     __metadata("design:returntype", Promise)
 ], UserResolver.prototype, "register", null);
+__decorate([
+    (0, type_graphql_1.Mutation)(() => UserResponse),
+    __param(0, (0, type_graphql_1.Arg)("options")),
+    __param(1, (0, type_graphql_1.Ctx)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [UsernamePasswordInput, Object]),
+    __metadata("design:returntype", Promise)
+], UserResolver.prototype, "login", null);
 UserResolver = __decorate([
     (0, type_graphql_1.Resolver)()
 ], UserResolver);
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,14 @@
 import { User } from "../entities/User";
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
 import argon2 from "argon2";
 
 @InputType()
@@ -11,19 +19,97 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: MyContext
-  ) {
+  ): Promise<UserResponse> {
+    if (options.username.length <= 2) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username length must be greater than 2.",
+          },
+        ],
+      };
+    }
+
+    if (options.password.length <= 8) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Password length must be atleast 8 characters.",
+          },
+        ],
+      };
+    }
+
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
-      username: options.username,
+      username: options.username.toLowerCase(),
       password: hashedPassword,
     });
+
+    try {
+      await em.persistAndFlush(user);
+    } catch (err) {
+      //duplicate username error
+      if (err.code === "23505" /*|| err.detail.includes("already exists")*/) {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "That username is not available.",
+            },
+          ],
+        };
+      }
+      console.log("message: ", err.message);
+    }
     await em.persistAndFlush(user);
-    return user;
+    return { user };
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, {
+      username: options.username.toLowerCase(),
+    });
+    if (!user) {
+      return {
+        errors: [{ field: "username", message: "Invalid Login." }],
+      };
+    }
+
+    const valid = await argon2.verify(user.password, options.password);
+    if (!valid) {
+      return {
+        errors: [{ field: "password", message: "Invalid Login." }],
+      };
+    }
+    return { user };
   }
 }
